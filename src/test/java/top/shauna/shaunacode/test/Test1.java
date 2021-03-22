2d071a324b83e6f737ab055f40c825d3f457c640
ShaunaChow <z1023778132@icloud.com>
2021-03-22 22:23:40 +0800

    'ok3'

--- src/test/java/top/shauna/shaunacode/test/Test1.java
+++ src/test/java/top/shauna/shaunacode/test/Test1.java
@@ -10,20 +10,24 @@ import top.shauna.shaunacode.ShaunaCode;
 public class Test1 {
 
     public static void main(String[] args) {
-        String source = "import top.shauna.shaunacode.ShaunaCode;\n" +
-                "\n" +
-                "/**\n" +
-                " * @Author Shauna.Chow\n" +
-                " * @Date 2021/3/21 21:18\n" +
-                " * @E-Mail z1023778132@icloud.com\n" +
-                " */\n" +
-                "public class Test1 {\n" +
-                "\n" +
-                "    public static void main(String[] args) {\n" +
-                "       System.out.println(12645); "+
-                "    }\n" +
-                "}";
-        System.out.println("000 "+ShaunaCode.remoteExecute(source));
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                String source = "import top.shauna.shaunacode.ShaunaCode;\n" +
+                        "\n" +
+                        "/**\n" +
+                        " * @Author Shauna.Chow\n" +
+                        " * @Date 2021/3/21 21:18\n" +
+                        " * @E-Mail z1023778132@icloud.com\n" +
+                        " */\n" +
+                        "public class Test1 {\n" +
+                        "\n" +
+                        "    public static void main(String[] args) {\n" +
+                        "       System.out.println(1);System.out.println(2);System.out.println(3);System.out.println(4); " +
+                        "    }\n" +
+                        "}";
+                System.out.println(ShaunaCode.remoteExecute(source));
+            }).start();
+        }
     }
 }
 
